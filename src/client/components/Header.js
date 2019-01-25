9ec67dc04616a312a931969e62f22ba4ea385c83
alexchomiak <alex.chomiak@gmail.com>
2019-01-25 04:10:05 -0600

    fixed grammar error

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -13,10 +13,10 @@ export default class Header extends React.Component {
 	render() {
 		return (
 			<div className="header">
-				<div className="container">
+				
 				<h1 className="header__title">  {this.props.title} </h1>	
 				<h3 className="header__subtitle"> {this.props.subtitle}</h3>
-				</div>
+				
 			</div>
 		)
 	}
